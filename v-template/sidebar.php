52c716a433a7f1f87d0408db124953fc9f40f509
vProjects <github@vyrazu.com>
2013-08-24 00:34:49 +0530

    chutiyapa

--- v-template/sidebar.php
+++
@@ -1,4 +0,0 @@
-<?php 
-	//get horizontal nav bar
-	$getAds->get_navbar_horizontal(); 
-?> 
--- v-template/sidebar_author.php
+++
@@ -1,6 +0,0 @@
-<div class="auth_info">
-    <h4>TODO</h4>
-    <ul>
-        <?php $article->getAuthor(); ?>
-    </ul>
-</div>
